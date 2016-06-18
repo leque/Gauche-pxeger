d5922eaad17ce49d20d4778f7dad020074617665
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-06-19 01:22:34 +0900

    run ci without sudo

--- test/travis-gauche.sh
+++ test/travis-gauche.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-sudo apt-get install -qq -y zlib1g-dev libgdbm-dev slib
-
 name="Gauche-${GAUCHE_VERSION}"
 source="$name.tgz"
 wget -O "$source" "http://prdownloads.sourceforge.net/gauche/$source"
 tar xzvf "$source"
-(cd $name; ./configure; make; sudo make install)
+(cd $name; ./configure --prefix=$HOME; make; make install)
